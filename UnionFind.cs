59a07894763f9289639ca7fee93c5c6ee1937382
ghj123h <confusions@outlook.com>
2024-08-05 20:03:38 +0800

    enhance dsu

--- UnionFind.cs
+++ UnionFind.cs
@@ -1,16 +1,21 @@
 public class DisjointSet {
+    private int n;
     private int[] fa;
     private void Update() {
         for (int i = 0; i < fa.Length; ++i) Find(i);
     }
 
-    public DisjointSet(int n) => fa = Enumerable.Range(0, n).ToArray();
-    public int Find(int n) => fa[n] == n ? fa[n] : fa[n] = Find(fa[n]);
+    public DisjointSet(int n) {
+        this.n = n;
+        fa = Enumerable.Range(0, n).ToArray();
+    }
+
+    public int Find(int u) => fa[u] == u ? fa[u] : fa[u] = Find(fa[u]);
     public void Merge(int x, int y) {
         x = Find(x);
         y = Find(y);
-        fa[x] = y;
+        if (x != y) { fa[x] = y; --n; }
     }
-    public int Count { get { Update(); return fa.Distinct().Count(); }}
+    public int Count { get => n; }
     public int Size(int x) { Update(); return fa.Where(t => t == fa[x]).Count(); }
 }
